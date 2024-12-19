1f6e9cedcafbca01371be4550d7a5ee921f533b8
lukasz-kkk <lukaluzny17@gmail.com>
2024-12-19 20:44:29 +0100

    Added child to database [migration]

---
+++ backend/Domain/Child.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Domain
+{
+    public class Child : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public int ParentId { get; set; }
+        public int GroupId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
