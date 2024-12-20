906d396824d713d4b7d6ef11f26eefab22fc5a8b
lukasz-kkk <lukaluzny17@gmail.com>
2024-12-20 19:49:40 +0100

    Add group to database [migration]

---
+++ backend/Domain/Group.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain
+{
+    public class Group : Entity
+    {
+        public string Name { get; set; }
+        public User Caregiver { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
