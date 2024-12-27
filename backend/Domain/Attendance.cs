65ff0e93785c37f38e3ea89c783699d100881e49
lukasz-kkk <lukaluzny17@gmail.com>
2024-12-27 23:53:28 +0100

    add attendance to db [migration]

---
+++ backend/Domain/Attendance.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class Attendance : Entity
+    {
+        public User Child { get; set; }
+        public DateOnly Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ backend/Infrastructure/AttendanceRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AttendanceRepository : Repository<Attendance>, IAttendanceRepository
+    {
+        public AttendanceRepository(AppDbContext appDbContext) 
+            : base(appDbContext, appDbContext.Attendances)
+        {
+        }
+    }
+}
