84c2e9eb8a930ea0ff03db0997a2c44b03850bee
lukasz-kkk <lukaluzny17@gmail.com>
2024-12-27 14:46:31 +0100

    Announcement mock

---
+++ backend/PrzedszkolePlus/Responses/AnnouncementResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace PrzedszkolePlus.Response
+{
+    public class AnnouncementResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string FilePath { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
