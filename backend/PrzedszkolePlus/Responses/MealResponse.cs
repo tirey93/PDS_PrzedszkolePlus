a28abfa1e4d096b0efb8cdac8e76ec9f04981493
lukasz-kkk <lukaluzny17@gmail.com>
2024-12-27 16:12:57 +0100

    Meal mock

---
+++ backend/PrzedszkolePlus/Responses/MealResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace PrzedszkolePlus.Response
+{
+    public class MealResponse
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public DateOnly Date { get; set; }
+        public string Breakfast { get; set; }
+        public string Lunch { get; set; }
+        public string Dinner { get; set; }
+    }
+}
