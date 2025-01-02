ceb8da9322af03a1f1aac01121d8e1faf52dad41
lukasz-kkk <lukaluzny17@gmail.com>
2025-01-02 21:29:42 +0100

    Attendance create

---
+++ backend/PrzedszkolePlus/Commands/CreateAttendanceCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PrzedszkolePlus.Commands
+{
+    public class CreateAttendanceCommand : IRequest<Unit>
+    {
+        public int ChildId { get; set; }
+        public DateOnly Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- backend/PrzedszkolePlus/Controllers/AttendanceController.cs
+++ backend/PrzedszkolePlus/Controllers/AttendanceController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PrzedszkolePlus.Commands;
+using PrzedszkolePlus.Exceptions;
+using PrzedszkolePlus.Properties;
 using PrzedszkolePlus.Requests;
 using PrzedszkolePlus.Response;
+using System.Net;
 
 namespace PrzedszkolePlus.Controllers
 {
@@ -8,18 +14,60 @@ namespace PrzedszkolePlus.Controllers
     [Route("[controller]")]
     public class AttendanceController : ControllerBase
     {
-        public AttendanceController()
+        private readonly IMediator _mediator;
+        public AttendanceController(IMediator mediator)
         {
-
+            _mediator = mediator;
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
 #if !DEBUG
         [Authorize(Roles = Roles.User)]
 #endif
-        public IActionResult Post([FromBody] AttendanceRequest dto)
+        public async Task<ActionResult> Post([FromBody] AttendanceRequest dto)
         {
-            return NoContent();
+            var request = new CreateAttendanceCommand
+            {
+                ChildId = dto.ChildId,
+                Date = dto.Date,
+                Status = dto.Status
+            };
+
+            try
+            {
+                await _mediator.Send(request);
+                return NoContent();
+            }
+            catch (ChildNotFoundException ex)
+            {
+                return StatusCode((int)HttpStatusCode.NotFound,
+                    string.Format(Resource.ControllerNotFound, ex.Message));
+            }
+            catch (AuthenticationFailureException ex)
+            {
+                return StatusCode((int)HttpStatusCode.Forbidden,
+                    string.Format(Resource.ControllerForbidden, ex.Message));
+            }
+            catch (InvalidCookieException ex)
+            {
+                return StatusCode((int)HttpStatusCode.BadRequest,
+                    string.Format(Resource.ControllerBadRequest, ex.Message));
+            }
+            catch (UserIsNotParentOfThisChildException ex)
+            {
+                return StatusCode((int)HttpStatusCode.Forbidden,
+                    string.Format(Resource.ControllerForbidden, ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError,
+                    string.Format(Resource.ControllerInternalError, ex.Message));
+            }
         }
 
         [HttpGet("ChildrenByLoggedUser")]
