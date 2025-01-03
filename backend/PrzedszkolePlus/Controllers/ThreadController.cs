a909aef86cf5928919660daa02f735d46639cfaa
tirey93 <tirey93@gmail.com>
2025-01-03 18:43:19 +0100

    Message Mock

---
+++ backend/PrzedszkolePlus/Controllers/ThreadController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrzedszkolePlus.Requests;
+using PrzedszkolePlus.Response;
+
+namespace PrzedszkolePlus.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ThreadController : ControllerBase
+    {
+        public ThreadController()
+        {
+
+        }
+
+        [HttpPost]
+#if !DEBUG
+        [Authorize(Roles = Roles.User)]
+#endif
+        public IActionResult Post([FromBody] ThreadRequest dto)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id:int}")]
+#if !DEBUG
+        [Authorize(Roles = Roles.User)]
+#endif
+        public IActionResult Put(int id, ThreadRequest dto)
+        {
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id:int}")]
+#if !DEBUG
+        [Authorize(Roles = Roles.User)]
+#endif
+        public ActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("ByLoggedUser")]
+#if !DEBUG
+        [Authorize(Roles = Roles.User)]
+#endif
+        public ActionResult<IEnumerable<ThreadResponse>> GetByLoggedUser()
+        {
+            return new List<ThreadResponse>
+            {
+                new ThreadResponse
+                {
+                    Id = 1,
+                    IsRead = true,
+                    ReceiverId = 12,
+                    Subject = "Subject1"
+                },
+                new ThreadResponse
+                {
+                    Id = 2,
+                    IsRead = false,
+                    ReceiverId = 13,
+                    Subject = "Subject2"
+                }
+            };
+        }
+
+    }
+}
