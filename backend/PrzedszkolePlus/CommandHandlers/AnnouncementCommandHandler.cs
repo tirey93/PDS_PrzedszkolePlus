a1c0a16d71f9ec5ec90fe9646c0ed5ee82691f35
lukasz-kkk <lukaluzny17@gmail.com>
2025-01-04 22:03:19 +0100

    announcements POST

---
+++ backend/PrzedszkolePlus/CommandHandlers/AnnouncementCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using PrzedszkolePlus.Commands;
+using MediatR;
+using Domain.Repositories;
+
+namespace PrzedszkolePlus.CommandHandlers
+{
+    public class AnnouncementCommandHandler : IRequestHandler<CreateAnnouncementCommand, Unit>
+    { 
+        private readonly IAnnouncementRepository _announcementRepository;
+
+        public AnnouncementCommandHandler(IAnnouncementRepository announcementRepository)
+        {
+            _announcementRepository = announcementRepository;
+        }
+
+        public async Task<Unit> Handle(CreateAnnouncementCommand request, CancellationToken cancellationToken)
+        {
+            var announcement = new Announcement
+            {
+                Title = request.Title,
+                Content = request.Content,
+                FilePath = request.FilePath,
+                CreatedAt = DateTime.Now
+            };
+
+            _announcementRepository.Add(announcement);
+            await _announcementRepository.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
