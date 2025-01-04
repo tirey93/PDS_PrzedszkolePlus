2cdfa28962ea6a367d7e1e43007bf7d5b135df1f
Damian Opolski <dapyt@eg.dk>
2025-01-04 12:17:28 +0100

    Messages Migration

---
+++ backend/Domain/Message.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain
+{
+    public class Message : Entity
+    {
+        public string Content { get; set; }
+        public User Sender { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ backend/Infrastructure/MessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using Domain.Repositories;
+
+namespace Infrastructure
+{
+    public class MessageRepository : Repository<Message>, IMessageRepository
+    {
+        public MessageRepository(AppDbContext appDbContext) 
+            : base(appDbContext, appDbContext.Messages)
+        {
+        }
+    }
+}
