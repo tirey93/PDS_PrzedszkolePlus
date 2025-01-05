fbcac04b1b174cab5b0df20d9a736b8f0a52bdca
Damian Opolski <dapyt@eg.dk>
2025-01-05 09:28:41 +0100

    Meal Migration

---
+++ backend/Infrastructure/MealRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using Domain.Repositories;
+
+namespace Infrastructure
+{
+    public class MealRepository : Repository<Meal>, IMealRepository
+    {
+        public MealRepository(AppDbContext appDbContext) 
+            : base(appDbContext, appDbContext.Meals)
+        {
+        }
+    }
+}
---
+++ backend/Infrastructure/Migrations/20250105082753_Meals.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Meals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GroupId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Breakfast = table.Column<string>(type: "TEXT", nullable: true),
+                    Lunch = table.Column<string>(type: "TEXT", nullable: true),
+                    Dinner = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Meals_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meals_GroupId",
+                table: "Meals",
+                column: "GroupId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meals");
+        }
+    }
+}
